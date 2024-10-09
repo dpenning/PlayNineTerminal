b7794cf24ba8d1e8bb7cac73b48e6f53855dbb2a
David Pennington <sokoban_commiter@dpenning.com>
2024-10-09 12:07:51 -0700

    move the init/deinit to game state

--- src/game.zig
+++ src/game.zig
@@ -1,59 +1,121 @@
 const std = @import("std");
 
-pub const CardValue = enum(i8) {
-    negative_five = -5,
-    zero = 0,
-    one,
-    two,
-    three,
-    four,
-    five,
-    six,
-    seven,
-    eight,
-    nine,
-    ten,
-    eleven,
-    twelve,
-};
-
 pub const Card = struct {
-    value: CardValue,
+    pub const Value = enum(i8) {
+        negative_five = -5,
+        zero = 0,
+        one,
+        two,
+        three,
+        four,
+        five,
+        six,
+        seven,
+        eight,
+        nine,
+        ten,
+        eleven,
+        twelve,
+    };
+
+    value: Value,
     flipped: bool,
 };
-pub const Deck = std.ArrayList(CardValue);
+pub const Deck = std.ArrayList(Card.Value);
 
-pub const PlayerType = enum {
-    local,
-    cpu,
-};
 pub const Player = struct {
-    control_type: PlayerType,
-    board: [4][2]Card,
-};
+    pub const Control = enum {
+        local,
+        cpu,
+    };
+    pub const Board = [4][2]Card;
 
+    control: Control,
+    board: Board,
+};
 pub const Players = std.ArrayList(Player);
 
 pub const GameState = struct {
+    const Self = @This();
+
     rng: std.Random,
     draw_pile: Deck,
-    current_card: CardValue,
+    current_card: Card.Value,
     discard: Deck,
     players: Players,
     current_player: u8,
+
+    pub fn init(
+        player_count: u8,
+        allocator: std.mem.Allocator,
+        rng: std.Random,
+    ) !Self {
+        var draw_pile = try GetNewDrawPile(allocator);
+        std.Random.shuffle(rng, Card.Value, draw_pile.items);
+
+        var players = try std.ArrayList(Player).initCapacity(
+            allocator,
+            player_count,
+        );
+
+        // Add the human Player.
+        players.insertAssumeCapacity(0, Player{
+            .board = BuildPlayerBoard(&draw_pile),
+            .control = Player.Control.local,
+        });
+
+        // Add the cpu Players.
+        for (1..player_count) |i| {
+            players.insertAssumeCapacity(i, Player{
+                .board = BuildPlayerBoard(&draw_pile),
+                .control = Player.Control.cpu,
+            });
+        }
+
+        const current_card = draw_pile.pop();
+
+        const discard = try GetNewDeck(allocator);
+
+        return GameState{
+            .rng = rng,
+            .draw_pile = draw_pile,
+            .current_card = current_card,
+            .current_player = 0,
+            .players = players,
+            .discard = discard,
+        };
+    }
+
+    pub fn deinit(self: Self) void {
+        defer _ = self.draw_pile.deinit();
+        defer _ = self.discard.deinit();
+        defer _ = self.players.deinit();
+    }
 };
 
-pub fn BuildPlayerBoard(deck: *Deck) Player.board {
-    return Player.board{
-        [2]Card{ deck.pop(), deck.pop() },
-        [2]Card{ deck.pop(), deck.pop() },
-        [2]Card{ deck.pop(), deck.pop() },
-        [2]Card{ deck.pop(), deck.pop() },
+pub fn BuildPlayerBoard(deck: *Deck) Player.Board {
+    return Player.Board{
+        [2]Card{
+            Card{ .value = deck.pop(), .flipped = false },
+            Card{ .value = deck.pop(), .flipped = false },
+        },
+        [2]Card{
+            Card{ .value = deck.pop(), .flipped = false },
+            Card{ .value = deck.pop(), .flipped = false },
+        },
+        [2]Card{
+            Card{ .value = deck.pop(), .flipped = false },
+            Card{ .value = deck.pop(), .flipped = false },
+        },
+        [2]Card{
+            Card{ .value = deck.pop(), .flipped = false },
+            Card{ .value = deck.pop(), .flipped = false },
+        },
     };
 }
 
 pub fn GetNewDeck(allocator: std.mem.Allocator) !Deck {
-    const card_count = (@typeInfo(CardValue).Enum.fields.len - 1) * 8 + 4;
+    const card_count = (@typeInfo(Card.Value).Enum.fields.len - 1) * 8 + 4;
     return try Deck.initCapacity(
         allocator,
         card_count,
@@ -63,11 +125,11 @@ pub fn GetNewDeck(allocator: std.mem.Allocator) !Deck {
 pub fn GetNewDrawPile(allocator: std.mem.Allocator) !Deck {
     var deck = try GetNewDeck(allocator);
     for (0..4) |i| {
-        deck.insertAssumeCapacity(i, CardValue.negative_five);
+        deck.insertAssumeCapacity(i, Card.Value.negative_five);
     }
 
     var insertion_index: u8 = 4;
-    inline for (@typeInfo(CardValue).Enum.fields[1..]) |field| {
+    inline for (@typeInfo(Card.Value).Enum.fields[1..]) |field| {
         for (0..8) |_| {
             deck.insertAssumeCapacity(
                 insertion_index,
@@ -80,12 +142,12 @@ pub fn GetNewDrawPile(allocator: std.mem.Allocator) !Deck {
     return deck;
 }
 
-pub fn DealCard(state: *GameState) CardValue {
-    if (state.deck.items.len() == 0) {
-        const tmp = state.deck;
-        state.deck = state.discard;
+pub fn DealCard(state: *GameState) Card.Value {
+    if (state.draw_pile.items.len == 0) {
+        const tmp = state.draw_pile;
+        state.draw_pile = state.discard;
         state.discard = tmp;
     }
 
-    return state.deck.pop();
+    return state.draw_pile.pop();
 }
--- src/main.zig
+++ src/main.zig
@@ -11,48 +11,18 @@ pub fn main() !void {
         try std.posix.getrandom(std.mem.asBytes(&seed));
         break :blk seed;
     });
-    const rand = prng.random();
 
     // Allocator.
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
     // Game State.
-    var game_state = try {
-        var draw_pile = try game.GetNewDrawPile(gpa.allocator());
-        shuffle(rand, game.CardValue, draw_pile.items);
+    var game_state = try game.GameState.init(
+        player_count,
+        gpa.allocator(),
+        prng.random(),
+    );
+    defer game_state.deinit();
 
-        var players = try std.ArrayList(game.Player).initCapacity(
-            gpa.allocator(),
-            player_count,
-        );
-
-        // Add the human Player.
-        players.insertAssumeCapacity(0, game.Player{
-            .board = game.BuildPlayerBoard(&draw_pile),
-            .control_type = game.PlayerType.player,
-        });
-
-        // Add the cpu Players.
-        for (1..players) |i| {
-            players.insertAssumeCapacity(i, game.Player{
-                .board = game.BuildPlayerBoard(&draw_pile),
-                .control_type = game.PlayerType.cpu,
-            });
-        }
-
-        const current_card = draw_pile.pop();
-
-        const game_state = try game.GameState{
-            .rng = prng.random(),
-            .draw_pile = draw_pile,
-            .current_card = current_card,
-            .current_player = 0,
-            .players = players,
-            .discard = try game.GetNewDeck(),
-        };
-        return game_state;
-    };
-
-    std.debug.print("{}\n", .{game.DealCard(&game_state)});
+    // Start Game Loop
 }
